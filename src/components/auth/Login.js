d486091964f70af8adde00c3cae4bfdd5a14f20e
Hulikner <hulikner@gmail.com>
2022-05-16 15:12:45 -0500

    readme

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,80 +2,70 @@ import React, { useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
 
-export const Login = ({setAuthUser}) => {
-	const [loginUser, setLoginUser ] = useState({ email: "" });
-	const [existDialog, setExistDialog] = useState(false);
+export const Login = ({ setAuthUser }) => {
+  const [loginUser, setLoginUser] = useState({ email: "" });
+  const [existDialog, setExistDialog] = useState(false);
 
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const handleInputChange = (event) => {
-		const newUser = { ...loginUser };
-		newUser[event.target.id] = event.target.value;
-		setLoginUser(newUser);
-	};
+  const handleInputChange = (event) => {
+    const newUser = { ...loginUser };
+    newUser[event.target.id] = event.target.value;
+    setLoginUser(newUser);
+  };
 
-	 const existingUserCheck = () => {
-		// If your json-server URL is different, please change it below!
-		return fetch(`http://localhost:8088/users?email=${loginUser.email}`)
-			.then((res) => res.json())
-			.then((user) => (user.length ? user[0] : false));
-	};
+  const existingUserCheck = () => {
+    return fetch(`http://localhost:8088/users?email=${loginUser.email}`)
+      .then((res) => res.json())
+      .then((user) => (user.length ? user[0] : false));
+  };
 
-	const handleLogin = (e) => {
-		e.preventDefault();
+  const handleLogin = (e) => {
+    e.preventDefault();
 
-		existingUserCheck().then((exists) => {
-			if (exists) {
-				setAuthUser(exists)
-				navigate("/home");
-			} else {
-				setExistDialog(true);
-			}
-		});
-	};
+    existingUserCheck().then((exists) => {
+      if (exists) {
+        setAuthUser(exists);
+        navigate("/home");
+      } else {
+        setExistDialog(true);
+      }
+    });
+  };
 
-	return (
-		<>
-		<main className="container--login">
-			<dialog className="dialog dialog--auth" open={existDialog}>
-				<div>User does not exist</div>
-				<button
-					className="button--close"
-					onClick={(e) => setExistDialog(false)}
-				>
-					Close
-				</button>
-			</dialog>
-			<section>
-				<form className="form--login" onSubmit={handleLogin}>
-					{/* <h1>Exo-Travel</h1> */}
-					<div className="login-logo-home">
-					<img  className="login-logo-home" src="/Images/Exo-Travel-Logo.svg" />
-					</div>
-					<h2>Sign In</h2>
-					<fieldset>
-						<label className="login-email" htmlFor="inputEmail"> Email Address</label>
-						<input
-							type="email"
-							id="email"
-							className="form-control"
-							placeholder="Email address"
-							required
-							autoFocus
-							value={loginUser.email}
-							onChange={handleInputChange}
-						/>
-					</fieldset>
-					<fieldset>
-						<button className="login-btn" type="submit">Sign in</button>
-					</fieldset>
-				</form>
-			</section>
-			<section className="link--register">
-				<Link to="/register">Register for an account</Link>
-			</section>
-		</main>
-		<div className="bottom--login"></div>
-	</>
-	);
+  return (
+    <>
+      <main className="container--login">
+        <dialog className="dialog dialog--auth" open={existDialog}>
+          <div>User does not exist</div>
+          <button className="button--close" onClick={(e) => setExistDialog(false)}>
+            Close
+          </button>
+        </dialog>
+        <section>
+          <form className="form--login" onSubmit={handleLogin}>
+            <div className="login-logo-home">
+              <img className="login-logo-home" src="/Images/Exo-Travel-Logo.svg" />
+            </div>
+            <h2>Sign In</h2>
+            <fieldset>
+              <label className="login-email" htmlFor="inputEmail">
+                Email Address
+              </label>
+              <input type="email" id="email" className="form-control" placeholder="Email address" required autoFocus value={loginUser.email} onChange={handleInputChange} />
+            </fieldset>
+            <fieldset>
+              <button className="login-btn" type="submit">
+                Sign in
+              </button>
+            </fieldset>
+          </form>
+        </section>
+        <section className="link--register">
+          <Link to="/register">Register for an account</Link>
+        </section>
+      </main>
+      <div className="bottom--login"></div>
+    </>
+  );
 };
